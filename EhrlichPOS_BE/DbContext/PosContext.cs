462967d648e6c83abd020b37dfc9fbb31cb0b78f
Aires Halili <haliliofficial@gmail.com>
2024-04-14 11:36:15 +0800

    Add project files.

---
+++ EhrlichPOS_BE/DbContext/PosContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+
+namespace EhrlichPOS_BE;
+
+public partial class PosContext : DbContext
+{
+
+    public PosContext()
+    {
+    }
+
+    public PosContext(DbContextOptions<PosContext> options) : base(options)
+    {
+
+    }
+
+}
+
+
---
+++ EhrlichPOS_BE/Program.cs
@@ -0,0 +1,35 @@
+using EhrlichPOS_BE;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = $"Data Source=.;Initial Catalog=EhrlichPOS; Integrated Security=True; TrustServerCertificate=True";
+builder.Services.AddDbContext<PosContext>(opt => opt.UseSqlServer(connectionString));
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
